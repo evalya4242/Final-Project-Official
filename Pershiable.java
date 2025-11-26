a7c3c953f75db56d27acf61093a495fe5cd20fde
evalya4242 <evalyakhevich07@gmail.com>
2025-11-26 18:59:36 +0400

    Update Pershiable.java

--- Pershiable.java
+++ Pershiable.java
@@ -1,26 +1,15 @@
-public class Perishableitem extends Item
-{
+public class PerishableItem extends Item {
+
     protected String expiryDate;
 
-    public String getExpiryDate()
-    {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(String expiryDate)
-    {
-        this.expiryDate = expiryDate;
-    }
-    public Perishableitem(String itemId, String itemmName, double price, int quantity, String expiryDate)
-    {
+    public PerishableItem(String itemId, String itemName, double price, int quantity, String expiryDate) {
         super(itemId, itemName, price, quantity);
         this.expiryDate = expiryDate;
     }
 
     @Override
-    public void displayItemInfo()
-    {
-        super.displayItemInfo();  
+    public void displayItemInfo() {
+        super.displayItemInfo();
         System.out.println("Expiry Date: " + expiryDate);
     }
 }
